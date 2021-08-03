9f1d72bfd5a4f1cc9519e3e5ae7e180663033669
Altzeus <88247068+AltTheZeus@users.noreply.github.com>
2021-08-03 18:08:39 +0300

    Add files via upload

--- Starstorm Lost/Survivors/scout.lua
+++ Starstorm Lost/Survivors/scout.lua
@@ -5,9 +5,9 @@ local path = "Survivors/Scout/"
 
 local scout = Survivor.new("Scout")
 
-local dronePath = "Drones/"
-require(dronePath.."SMGdrone")
-require(dronePath.."Laserdrone")
+--local dronePath = "Drones/"
+--require(dronePath.."SMGdrone")
+--require(dronePath.."Laserdrone")
 
 -- Scout
 
@@ -77,7 +77,7 @@ scout.loadoutColor = Color.fromHex(0x43DBB0)
 scout.idleSprite = sprites.idle
 
 -- Main menu sprite
-scout.titleSprite = sprites.jumpHover
+scout.titleSprite = sprites.walk
 
 -- Endquote
 scout.endingQuote = "..and so they left, radars and scanners still pinging onwards."
@@ -146,6 +146,7 @@ end)
 
 -- Skills
 
+-- BOMB 
 local objBomb = Object.new("ScoutBomb")
 local mask = Sprite.load("Scout_BombMask", path.."bombMask", 1, 2, 2)
 objBomb.sprite = Sprite.load("Scout_Bomb", path.."bomb", 10, 5, 5)
@@ -214,6 +215,293 @@ objBomb:addCallback("step", function(self)
 	end
 end)
 
+-- SMG DRONE
+local smgIdle = Sprite.load("SMGDroneIdle", path.."SMGIdle", 4, 6, 10)
+obj.smgdrone = Object.new("SMGDrone")
+obj.smgdrone.sprite = smgIdle
+
+obj.smgdrone:addCallback("create", function(self)
+	local selfData = self:getData()
+	self.spriteSpeed = 0.2
+	selfData.cooldown = 10
+	selfData.attackTimerMax = 6 * 4
+	selfData.attackTimer = selfData.attackTimerMax
+	selfData.state = "idle"
+	selfData.heat = 0
+	selfData.location = 1
+	selfData.yOffset = 0
+end)
+
+obj.smgdrone:addCallback("step", function(self)
+	local selfData = self:getData()
+	local selfAc = self:getAccessor()
+	local parent = Object.findInstance(selfData.parentId)
+	
+	if parent and parent:isValid() and parent:get("dead") == 0 then
+		-- Awesome's code here
+		if math.chance(50) then
+			selfData.yOffset = selfData.yOffset + 1/30
+		end
+		if selfData.yOffset > math.pi then
+			selfData.yOffset = -math.pi
+		end
+		local offset = math.sin(selfData.yOffset) * 5
+		-- thanks for making drone idle more interesting! :)
+		
+		if selfData.state == "idle" then
+			local xx = parent.x + 12 * selfData.location - self.x
+			local yy = parent.y - 8 - offset - self.y
+			self.x = math.approach(self.x, parent.x + 12 * selfData.location, xx * 0.1)
+			self.y = math.approach(self.y, parent.y - 8 - offset, yy * 0.1)
+			self.xscale = parent.xscale
+			if selfData.heat >= 75 then
+				selfData.state = "attack"
+			end
+		end
+			
+		if selfData.heat > 0 then
+			selfData.heat = selfData.heat - 1
+		end
+		
+		if selfData.state == "attack" then
+			local r = 100
+			local target = parent
+			local dis = r * 3
+           	for _, actor in ipairs(pobj.actors:findAllEllipse(self.x + r * 2, self.y + r, self.x - r * 2, self.y - r)) do
+               	if actor and actor:isValid() and actor:get("team") ~= parent:get("team") then
+                    if distance(self.x, self.y, actor.x, actor.y) < dis then
+                        target = actor
+                      	dis = distance(self.x, self.y, actor.x, actor.y)
+                  	end
+                end
+            end
+			
+            if target and target ~= parent then
+				local xx = target.x - self.x 
+				local yy = target.y - self.y
+				
+				
+				self.y = math.approach(self.y, target.y, yy * 0.1)
+				
+				if xx ~= 0 then
+					self.xscale = math.sign(xx)
+				end
+				
+				if selfData.cooldown > 0 and math.chance(50) then
+					selfData.cooldown = selfData.cooldown - 1
+				else
+					if selfData.attackTimer > 0 then
+						if selfData.attackTimer % 4 == 0 then
+							sfx.ChildDeath:play(2.4 + math.random(-2, 3) * 0.1, 0.4)
+							misc.fireBullet(self.x, self.y, self.xscale * -90 + 90, 100, parent:get("damage") * 0.2, parent:get("team"), Sprite.find("Scout_Sparks1", "SSLost")):set("specific_target", target.id)
+							local chance = 0
+							for i = 1, parent:getData().armsrace do
+								chance = math.approach(chance, 100, (100 - chance) * 0.05)
+							end
+							if math.chance(chance) then
+								obj.EfMissileSmall:create(self.x, self.y):set("parent", self.id):set("damage", parent:get("damage"))
+							end
+						end
+						selfData.attackTimer = selfData.attackTimer - 1
+					else
+						selfData.cooldown = 60
+						selfData.attackTimer = selfData.attackTimerMax
+					end
+					
+				end
+			else
+				selfData.state = "idle"
+			end
+			if selfData.heat == 0 and selfData.cooldown > 0 then
+				selfData.state = "idle"
+			end
+		end
+	end
+end)
+
+local armsraceSpr = Sprite.find("EfArmsrace", "Vanilla")
+obj.smgdrone:addCallback("draw", function(self)
+	local parent = Object.findInstance(self:getData().parentId)
+	if parent:getData().armsrace > 0 then
+		graphics.drawImage{
+		image = armsraceSpr,
+		x = self.x,
+		y = self.y,
+		xscale = self.xscale
+		}
+	end
+end)
+
+local armsrace = Item.find("Arms Race", "Vanilla")
+armsrace:addCallback("pickup", function(player)
+	if player:getSurvivor() == Survivor.find("Scout", "SSlost") then
+		player:getData().armsrace = player:getData().armsrace + 1
+	end
+end)
+
+callback.register("onItemRemoval", function(player, item, amount)
+	if item == armsrace and player:getSurvivor() == Survivor.find("Scout", "SSlost") then
+		player:getData().armsrace = player:getData().armsrace - amount
+	end
+end)
+-- SMG DRONE END 
+
+--LASER DRONE
+local laserIdle = Sprite.load("LaserDroneIdle", path.."SMGIdle", 4, 6, 10)
+obj.laserdrone = Object.new("LaserDrone")
+obj.laserdrone.sprite = laserIdle
+
+obj.laserdrone:addCallback("create", function(self)
+	local selfData = self:getData()
+	self.spriteSpeed = 0.2
+	selfData.state = "idle"
+	selfData.direction = {
+	spin = 1,
+	laserStart = 0,
+	laserEnd = 180
+	}
+	selfData.scepter = 0
+end)
+
+obj.laserdrone:addCallback("step", function(self)
+	local selfData = self:getData()
+	local selfAc = self:getAccessor()
+	local parent = Object.findInstance(selfData.parentId)
+	
+	if not selfData.yloc then
+		selfData.yloc = parent.y - 65
+	end
+	
+	if parent and parent:isValid() and parent:get("dead") == 0 then
+	
+		if selfData.state == "idle" then
+			local yy = selfData.yloc - self.y
+			self.y = math.approach(self.y, selfData.yloc, yy * 0.1)
+			if math.round(self.y) == math.round(selfData.yloc) then
+				selfData.state = "attack"
+			end
+		end
+		
+		self.angle = (global.timer % 30) * -12 * selfData.direction.spin
+		
+		if selfData.state == "attack" then
+			if not selfData.laser then
+				selfData.laser = selfData.direction.laserStart
+			end
+			local r = 180
+			local boom = false
+			local direction = math.rad(selfData.laser)
+			while r > 0 do
+				selfData.xLaser = self.x + math.cos(direction) * r
+				selfData.yLaser = self.y + math.sin(direction) * r
+				local tile = obj.B:findLine(self.x, self.y, selfData.xLaser, selfData.yLaser) or obj.BNoSpawn:findLine(self.x, self.y, selfData.xLaser, selfData.yLaser)
+				if tile then
+					r = r - 1
+					boom = true
+				else
+					break
+				end
+			end
+			
+			while r > 0 do
+				local actor = pobj.actors:findLine(self.x, self.y, selfData.xLaser, selfData.yLaser)
+				selfData.xLaser = self.x + math.cos(direction) * r
+				selfData.yLaser = self.y + math.sin(direction) * r
+				if actor and actor:isValid() and actor:get("team") ~= parent:get("team") then
+					r = r - 1
+					boom = true
+				else
+					break
+				end
+			end
+			
+			if boom then 
+				sfx.GiantJellyExplosion:play(1.7, 0.2)
+				misc.fireExplosion(selfData.xLaser, selfData.yLaser, 16/19, 16/4, parent:get("damage") * ((r + 90) / 270), parent:get("team"), spr.EfExplosive, nil)
+			end
+			
+			if selfData.laser ~= selfData.direction.laserEnd then
+				selfData.laser = math.approach(selfData.laser, selfData.direction.laserEnd, 4)
+			else
+				selfData.laser = nil
+				if selfData.scepter > 0 then
+					selfData.direction.spin = selfData.direction.spin * -1
+					selfData.direction.laserStart = 90 + selfData.direction.spin * -90
+					selfData.direction.laserEnd = 90 + selfData.direction.spin * 90
+					selfData.laser = nil
+					selfData.scepter = selfData.scepter - 1
+				else
+					selfData.state = "pulse"
+				end
+			end
+		end
+		
+		if selfData.state == "pulse" then
+			if not selfData.pulse then
+				selfData.pulse = 60
+			end
+			if math.round(selfData.pulse) > 0 then
+				selfData.pulse = math.approach(selfData.pulse, 0, selfData.pulse * 0.05)
+			else
+				selfData.pulse = nil
+				selfData.state = "retract"
+			end
+		end
+		
+		if selfData.state == "retract" then 
+			local xx = self.x - parent.x
+			local yy = self.y - parent.y
+			self.x = math.approach(self.x, parent.x, xx * 0.05 + math.sign(xx) * 2)
+			self.y = math.approach(self.y, parent.y, yy * 0.05 + math.sign(yy) * 2)
+			local r = 10
+			if self.x > parent.x - r and self.x < parent.x + r and self.y > parent.y - r and self.y < parent.y + r then
+				self:destroy()
+			end
+		end
+	end
+end)
+
+obj.laserdrone:addCallback("draw", function(self)
+	local selfData = self:getData()
+	
+	if selfData.state == "pulse" then
+		local tele = nearestMatchingOp(self, obj.Teleporter, "isBig", "~=", 1)
+		graphics.color(Color.fromHex(0x43DBB0))
+		graphics.alpha(selfData.pulse / 100 + 0.2)
+		graphics.circle(self.x, self.y, 60 - selfData.pulse, true)
+		
+			local r = (60 - selfData.pulse) * 0.75
+			local direction = math.random(0, 360)
+			if tele then
+				direction = posToAngle(self.x, self.y, tele.x, tele.y, false)
+			end
+			local circlex = self.x + math.cos(math.rad(direction)) * r 
+			local circley = self.y - math.sin(math.rad(direction)) * r
+			--local circlex, circley = pointInLine(self.x, self.y, tele.x, tele.y, r)
+			--graphics.line(self.x, self.y, circlex, circley)
+			
+			local dir1 = math.rad(direction + 10)
+			local xx1 = self.x + math.cos(dir1) * r * 0.8
+			local yy1 = self.y - math.sin(dir1) * r * 0.8
+			
+			local dir2 = math.rad(direction - 10)
+			local xx2 = self.x + math.cos(dir2) * r * 0.8
+			local yy2 = self.y - math.sin(dir2) * r * 0.8
+			
+			graphics.triangle(circlex, circley, xx1, yy1, xx2, yy2, false)
+	end
+	
+	if selfData.state == "attack" then
+		if selfData.xLaser and selfData.yLaser then
+			graphics.color(Color.fromHex(0x43DBB0))
+			graphics.alpha(0.8)
+			graphics.line(self.x, self.y, selfData.xLaser, selfData.yLaser, 5)
+		end
+	end
+end)
+-- LASER DRONE END 
+
+-- ALL OTHER STUFF
 scout:addCallback("useSkill", function(player, skill)
 	local playerData = player:getData()
 	local playerAc = player:getAccessor()
