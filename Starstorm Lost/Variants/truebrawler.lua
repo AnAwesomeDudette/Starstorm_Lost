5ea53fc2b7a6ad022bedd39aab9c16052d89afef
AnAwesomeDudette <88060031+AnAwesomeDudette@users.noreply.github.com>
2021-08-08 03:29:35 -0500

    Add files via upload

--- Starstorm Lost/Variants/truebrawler.lua
+++ Starstorm Lost/Variants/truebrawler.lua
@@ -18,9 +18,9 @@ SurvivorVariant.setLoadoutSkill(newVariant, "Heavy Punch", "Down->Downforward->F
 SurvivorVariant.setLoadoutSkill(newVariant, "Skyward", "Down->Forward->Downforward+X to perform an invincible uppercut, hitting three times and launching opponents upwards for up to &y&700% damage.&!&", sprSkills, 2)
 SurvivorVariant.setLoadoutSkill(newVariant, "Charged Pounce", "Down->Neutral->Down+C to prepare to launch after 1 second. Pressing C further times &b&increases speed.&!& Enemies struck receive your momentum.", sprSkills, 3)
 SurvivorVariant.setLoadoutSkill(newVariant, "Ultra Suplex Hold", "Back->Downback->Down->Downforward->Forward+V to &y&grapple&!& the nearest foe, pummeling them mid-air for &y&100% damage&!& and slamming for &y&250% damage.&!&", sprSkills, 4)
-SurvivorVariant.setLoadoutSkill(newVariant, "One-Two", "Z->Z to peform a second punch quickly after the first. Can be cancelled into special moves.", sprSkills, 5)
-SurvivorVariant.setLoadoutSkill(newVariant, "Three-Hit Jab Combo", "Z->Z->Z to peform a fast three combination attack. The final hit stuns enemies for 150% damage.", sprSkills, 6)
-SurvivorVariant.setLoadoutSkill(newVariant, "Pursuit", "Z->X->C to perform a great leap. If an enemy was thrown, leap higher to chase them into the air.", sprSkills, 7) 
+SurvivorVariant.setLoadoutSkill(newVariant, "One-Two", "Z->Z to peform a second punch quickly after the first. &b&Special moves can be done quickly after the second blow.&!&", sprSkills, 5)
+SurvivorVariant.setLoadoutSkill(newVariant, "Three-Hit Jab Combo", "Z->Z->Z to peform a fast three combination attack. The final hit &b&stuns enemies&!& for &y&150% damage.&!&", sprSkills, 6)
+SurvivorVariant.setLoadoutSkill(newVariant, "Pursuit", "Z->X->C to perform a &b&great leap.&!& If an enemy was thrown, &b&leap higher to chase them into the air.&!&", sprSkills, 7) 
 
 --local skillSprite = Sprite.load("BluemandoSkill1", "Bluemando/Skill1", 1, 0, 0)
 --SurvivorVariant.setLoadoutSkill(newVariant, "Doublue Tab", "Shoot twice for &y&2x60% &b&blue &y&damage.&!&", skillSprite)
