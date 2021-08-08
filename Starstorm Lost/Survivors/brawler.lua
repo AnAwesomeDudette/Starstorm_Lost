0473929edd1283acac7991aa2f3475087dd3bdd4
AnAwesomeDudette <88060031+AnAwesomeDudette@users.noreply.github.com>
2021-08-08 01:12:11 -0500

    Add files via upload

--- Starstorm Lost/Survivors/brawler.lua
+++ Starstorm Lost/Survivors/brawler.lua
@@ -46,19 +46,18 @@ Despite his abilities, the Brawler believes that &lt&Not every situation needs s
 -- Skill descriptions
 
 Brawler:setLoadoutSkill(1, "Punch",
-[[Punch enemies at close range for 100% damage. Press three times 
-consecutively for a finisher, stunning enemies for 150% damage.]])
+[[Punch enemies at close range for &y&100% damage.&!&]])
 
 Brawler:setLoadoutSkill(2, "Throw",
-[[Throw enemies upwards for 250% damage.]])
+[[Throw enemies upwards for &y&250% damage.&!&]])
 
 Brawler:setLoadoutSkill(3, "Pounce",
-[[Launch yourself towards the nearest enemy in front of you.
-Deal 250% damage on impact with the enemy.]])
+[[&b&Launch yourself&!& towards the nearest enemy in front of you.
+Deal &y&250% damage&!& on impact with the enemy.]])
 
 Brawler:setLoadoutSkill(4, "Dive Drop",
-[[Become airborne and drop to the ground dealing up to 1000% damage.
-Deals more damage the higher the drop is executed. Stuns enemies.]])
+[[Become airborne and drop to the ground dealing up to &y&1000% damage.&!&
+Deals more damage the &b&higher the drop&!& is executed. &b&Stuns enemies.&!&]])
 
 -- Color of highlights during selection
 Brawler.loadoutColor = Color.fromHex(0xEAB779)
@@ -77,12 +76,12 @@ callback.register("postLoad", function() -- AWESOME LOOK HERE
 	SurvivorVariant.setDescription(SurvivorVariant.getSurvivorDefault(Brawler), "Wrestled with a bear bare-fisted and, well, bear was buried with utmost respect.")
 	if modloader.checkFlag("ssl_debug") then
 		SurvivorVariant.setLoadoutSkill(SurvivorVariant.getSurvivorDefault(Brawler), "Heavy Punch", "Down->Downforward->Forward+Z to perform a heavy punch, severely stunning opponents for &y&300% damage.&!& &g&(Lower zoom scale to see all Special Moves.)&!&", sprSkills, 1)
-		SurvivorVariant.setLoadoutSkill(SurvivorVariant.getSurvivorDefault(Brawler), "Skyward", "Down->Forward->Downforward+X to perform an invincible uppercut, hitting three times and launching opponents upwards for up to &y&700% damage.&!&", sprSkills, 2)
+		SurvivorVariant.setLoadoutSkill(SurvivorVariant.getSurvivorDefault(Brawler), "Skyward", "Forward->Down->Downforward+X to perform an invincible uppercut, hitting three times and launching opponents upwards for up to &y&700% damage.&!&", sprSkills, 2)
 		SurvivorVariant.setLoadoutSkill(SurvivorVariant.getSurvivorDefault(Brawler), "Charged Pounce", "Down->Neutral->Down+C to prepare to launch after 1 second. Pressing C further times &b&increases speed.&!& Enemies struck receive your momentum.", sprSkills, 3)
 		SurvivorVariant.setLoadoutSkill(SurvivorVariant.getSurvivorDefault(Brawler), "Ultra Suplex Hold", "Back->Downback->Down->Downforward->Forward+V to &y&grapple&!& the nearest foes, leaping to pummel them mid-air for &y&100% damage&!& and slam them down for &y&250% damage.&!&", sprSkills, 4)
-		SurvivorVariant.setLoadoutSkill(SurvivorVariant.getSurvivorDefault(Brawler), "One-Two", "Z->Z to peform a second punch quickly after the first. Can be cancelled into special moves.", sprSkills, 5)
-		SurvivorVariant.setLoadoutSkill(SurvivorVariant.getSurvivorDefault(Brawler), "Three-Hit Jab Combo", "Z->Z->Z to peform a fast three combination attack. The final hit stuns enemies for 150% damage.", sprSkills, 6)
-		SurvivorVariant.setLoadoutSkill(SurvivorVariant.getSurvivorDefault(Brawler), "Pursuit", "Z->X->C to perform a great leap. If an enemy was thrown, leap higher to chase them into the air.", sprSkills, 7)
+		SurvivorVariant.setLoadoutSkill(SurvivorVariant.getSurvivorDefault(Brawler), "One-Two", "Z->Z to peform a second punch quickly after the first. &b&Can be cancelled into special moves.&!&", sprSkills, 5)
+		SurvivorVariant.setLoadoutSkill(SurvivorVariant.getSurvivorDefault(Brawler), "Three-Hit Jab Combo", "Z->Z->Z to peform a fast three combination attack. The final hit &b&stuns enemies&!& for &y&150% damage.&!&", sprSkills, 6)
+		SurvivorVariant.setLoadoutSkill(SurvivorVariant.getSurvivorDefault(Brawler), "Pursuit", "Z->X->C to perform a &b&great leap.&!& If an enemy was thrown, &b&leap higher to chase them into the air.&!&", sprSkills, 7)
 	end
 
 end) --BASED DEPARTMENT
@@ -150,16 +149,16 @@ Brawler:addCallback("init", function(player)
 		player:survivorSetInitialStats(100, 13, 0.008)
 	end
 	
-	player:setSkill(1, "", "",
+	player:setSkill(1, "Punch", "Punch enemies at close range for 100% damage.",
 	sprSkills, 1, 25)
 		
-	player:setSkill(2, "", "",
+	player:setSkill(2, "Throw", "Throw enemies upwards for 250% damage.",
 	sprSkills, 2, 2 * 60)
 		
-	player:setSkill(3, "", "",
+	player:setSkill(3, "Pounce", "Launch yourself towards the nearest enemy in front of you. Deal 250% damage on impact with the enemy.",
 	sprSkills, 3, 3 * 60)
 		
-	player:setSkill(4, "", "",
+	player:setSkill(4, "Dive Drop", "Become airborne and drop to the ground dealing up to 1000% damage. Deals more damage the higher the drop is executed. Stuns enemies.",
 	sprSkills, 4, 4 * 60)
 end)
 
@@ -172,10 +171,10 @@ end)
 -- Called when the player picks up the Ancient Scepter
 Brawler:addCallback("scepter", function(player)
 	player:setSkill(4,
-		"",
-		"",
+		"True Buster",
+		"On True Brawler, Ultra Suplex Hold can now be performed on most bosses.",
 		sprSkills, 9,
-		9 * 60
+		3 * 60
 	)
 	player:getData().canBusterBosses = true
 end)
